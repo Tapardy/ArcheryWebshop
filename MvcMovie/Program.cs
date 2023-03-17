99cdb57b0087c964ea64a0c6864db3c54c60ecc6
Tapardy <117444657+Tapardy@users.noreply.github.com>
2023-03-17 11:24:17 +0100

    Update

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,8 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +12,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
